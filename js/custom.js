fae2dfb1419cd299e566c5deb5d97272b2ec3d71
Chizzy Justin <nnabugwuchizitelu@gmail.com>
2023-07-21 17:52:36 +0100

    add and subtract buttons added

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,7 @@
 var text;
 var cartList = [];
 var cartListBtnIdList = [];
+var itemQty = [];
 class Cart {
         constructor(amount, qty, itemName, itemPic, btnid) {
           this.amount = amount;
@@ -18,30 +19,60 @@ function addToCart(a,q,i,p,btnid){
         cartListBtnIdList.push(btnid);
         document.getElementById(btnid).disabled = true; 
       	console.log(cartList.length);
-   
+        itemQty.push(q);
 }
 
 function viewCart(){
     	   document.getElementById("xx").innerHTML = "";
          document.getElementById("numberOfItems").innerHTML = cartList.length + " " + "items"
         for (i=0; i < cartList.length; i++){
+        
          console.log(Number(cartList[i].amount))
         text = '<div class="row border-top border-bottom">'+
           '<div class="row main align-items-center">'+
                '<div class="col-2"><img class="img-fluid" src="images/'+cartList[i].itemPic+'" id="itemPicture"></div><div class="col"><div class="row text-muted">Shirt</div><div class="row">'+cartList[i].itemName+'</div></div>'+
-               '<div class="col"> <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a> </div>'+
-               '<div class="col">$'+cartList[i].amount+' <span class="close" id="remove" onclick="deleteCartItem('+i+')">&#10005;</span></div>'+
+               '<div class="col">' +
+               '<a href="#" onclick="reduce('+i+')">-</a>' +
+               '<a href="#" id="item-count'+i+'" class="border">'+itemQty[i]+'</a>' +
+               '<a href="#" onclick="increase('+i+')">+</a> </div>' +
+               '<div class="col">$'+cartList[i].amount *itemQty[i] +' <span class="close" id="remove" onclick="deleteCartItem('+i+')">&#10005;</span></div>'+
            '</div>'
         '</div>';
         document.getElementById("xx").innerHTML += text;
         }
         updatePrices();
 }
+function reduce(i){
+  var itemCount = document.getElementById("item-count"+i).innerHTML
+  console.log("reduce " +itemCount)
+  if (itemCount == 1) {
+    deleteCartItem(i);
+    updatePrices();
+  } else {
+    itemQty[i] = itemQty[i]-1;
+
+   viewCart();
+  }
+}
+
+function increase(i){
+  
+  var itemCount = document.getElementById("item-count"+i).innerHTML;
+  
+  itemQty[i] = itemQty[i]+1;
+ 
+  console.log("increase " +itemCount);
+  
+  viewCart();
+}
 
 function updatePrices(){
   var subTotals = 0;
+  
       for (i=0; i < cartList.length; i++){
-          subTotals += Number(cartList[i].amount);
+        
+        
+          subTotals += Number(cartList[i].amount) * itemQty[i];
           document.getElementById("subTotal").innerHTML = subTotals.toFixed(2) ;
           var vats  =107.5/100;
           var Total =  subTotals * vats;
@@ -64,6 +95,7 @@ function onClearCartClicked(){
 var btnId;
 function deleteCartItem(index){
         cartList.splice(index,1);
+        itemQty.splice(index, 1);
         btnId = cartListBtnIdList[index];
         document.getElementById(btnId).disabled = false;
         cartListBtnIdList.splice(index,1);
@@ -81,6 +113,8 @@ function clearCart(){
   }
   cartList.splice(0,cartList.length);
   cartList.length = 0;
+  itemQty = [];
+
  
   viewCart();
   onClearCartClicked();
